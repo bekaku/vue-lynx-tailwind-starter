80c2bb71dbeef770e14334a8b0ed87f4f66eb6bd
bekaku <33171470+bekaku@users.noreply.github.com>
2026-05-18 14:53:00 +0700

    Update Padding.vue

--- src/components/test/Padding.vue
+++ src/components/test/Padding.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-const props = defineProps<{ class?: string }>()
+const props = defineProps<{ class?: string }>();
 </script>
 
 <template>
-  <view :class="props.class" :style="{border:'1px solid red'}">
+  <view
+    :class="props.class"
+    :style="{ border: '1px solid red', paddingTop: 0, paddingBottom: 0 }"
+  >
     <slot />
   </view>
 </template>
