bbed854efc8343fbbc631063ca9d016944979ee8
bekaku <33171470+bekaku@users.noreply.github.com>
2026-05-19 08:05:17 +0700

    .

--- src/composables/useDevice.ts
+++ src/composables/useDevice.ts
@@ -2,8 +2,8 @@ export const useDevice = () => {
     const isAndroid = SystemInfo.platform === 'Android';
     const isIos = SystemInfo.platform === 'iOS';
     const isWeb = SystemInfo.platform === 'web';
+    const isNative = SystemInfo.platform === 'iOS' || SystemInfo.platform === 'Android';
     const platform = SystemInfo.platform;
-    const isNative = isIos || isAndroid;
     const isNativeAvailable = typeof NativeModules !== 'undefined' && !!NativeModules.NativeLocalStorageModule;
 
     return {
